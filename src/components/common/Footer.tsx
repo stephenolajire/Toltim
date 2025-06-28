81e1e544931245bf6d554477444f8b79849808a9
olajirestephen <horlharmighty2000@gmail.com>
2025-06-28 07:20:13 -0600

    changed project title

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ import { BiHeart } from "react-icons/bi";
 const Footer: React.FC = () => {
   return (
     <footer className="w-full bg-black shadow-md px-2 sm:px-4 md:px-20 lg:px-50 py-4 h-[40vh] overflow-hidden">
-      <div className="flex flex-col items-center justify-center h-full w-full space-y-4">
+      <div className="flex flex-col items-center justify-center h-full w-full space-y-4 px-4 md:px-0">
         <div className="flex items-center h-auto">
           <BiHeart className="text-green-600 text-3xl md:text-4xl lg:text-6xl" />
           <span className="text-xl text-white font-bold ml-0.5 sm:ml-2">Toltimed</span>
